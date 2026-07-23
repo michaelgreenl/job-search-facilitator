9814fc0e074940ad6f0e9cc484dde153fa342717
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2026-07-23 15:39:18 -0400

    refactor: shrink icon adjustment

--- packages/app/client/src/components/svgs/ShrinkIcon.vue
+++ packages/app/client/src/components/svgs/ShrinkIcon.vue
@@ -1,6 +1,6 @@
 <template>
     <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-        <polyline points="4 10 10 10 10 4" />
-        <polyline points="20 14 14 14 14 20" />
+        <polyline points="2 8 8 8 8 2" />
+        <polyline points="22 16 16 16 16 22" />
     </svg>
 </template>
